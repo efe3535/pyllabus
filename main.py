bbf074ca32d6de635e063ea76a9084c08568f6bf
efe3535 <a.efe.akyazi@gmail.com>
2021-12-24 21:02:58 +0300

    Fix not working on the weekends.

--- main.py
+++ main.py
@@ -10,7 +10,9 @@ days_and_indexes = {
     'Tuesday':1,
     'Wednesday':2,
     'Thursday':3,
-    'Friday':4
+    'Friday':4,
+    'Saturday':5,
+    'Sunday':6
 }
 
 dayindex = days_and_indexes[datetime.now().strftime("%A")]
@@ -21,19 +23,29 @@ try:
     f.seek(0)
     tomorrows_classes = f.readlines()[dayindex + 1].split()
 
-except IndexError: # Means that the day is Friday, so it should show the first day: Monday. 
+except IndexError: # Means that the day is Friday or weekend, so it should show the first day: Monday. 
     f.seek(0)
     tomorrows_classes = f.readlines()[0].split()
 
 f.seek(0)
 
-todays_classes = f.readlines()[dayindex].split()
+todays_classes = f.readlines()[4].split()
+# Set Friday as default, if there is an error the day will be set to Friday.
+
+try:
+    f.seek(0)
+    todays_classes = f.readlines()[dayindex].split()
+except IndexError: # Weekend, we need to use the  
+    f.seek(0)
+    todays_classes = f.readlines()[4].split()
 
 cikarilacaklar = []
 odd_c = []
 koyulacaklar = []
 odd_k = []
 printed = []
+
+f.seek(0)
 
 for lesson in todays_classes:
     if lesson not in tomorrows_classes:
@@ -47,8 +59,8 @@ def rmodd(x):
   return list(dict.fromkeys(x))
 
 def ders_command(update: Update, context: CallbackContext):
-    koyulacaklar_formatted = "".join([x + "\n" for x in rmodd(koyulacaklar)])
-    cikarilacaklar_formatted = "".join([y + "\n" for y in rmodd(cikarilacaklar)])
+    koyulacaklar_formatted = "".join([x  + "\n" for x in rmodd(koyulacaklar)])
+    cikarilacaklar_formatted = "".join([y  + "\n" for y in rmodd(cikarilacaklar)])
     update.message.reply_text(f"Cantana koyman gerekenler:\n {koyulacaklar_formatted}\nCantandan cikarman gerekenler:\n {cikarilacaklar_formatted}")
 
 # print(rmodd(koyulacaklar), rmodd(cikarilacaklar))
